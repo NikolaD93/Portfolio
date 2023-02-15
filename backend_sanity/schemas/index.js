6ac4e1d171870c439d3ae8fe36640f06310f90f5
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-02-15 18:05:12 +0100

    initial commit

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,17 @@
+import works from './works'
+import testimonials from './testimonials'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  abouts,
+  experiences,
+  skills,
+  workExperience,
+  contact,
+]
